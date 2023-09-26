1a9973616b8bb3f8279aaca33f300584bd08afd7
Joao Teixeira <joao.teixeira.dev@gmail.com>
2023-09-26 19:32:59 -0300

    Product Model

---
+++ backoffice-api-java/src/main/java/com/backoffice_api_java/api/model/Product.java
@@ -0,0 +1,50 @@
+package com.backoffice_api_java.api.model;
+
+public class Product {
+    
+    private Integer id;
+    private Integer quantity;
+    private String name;
+    private String description;
+    private Double price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
