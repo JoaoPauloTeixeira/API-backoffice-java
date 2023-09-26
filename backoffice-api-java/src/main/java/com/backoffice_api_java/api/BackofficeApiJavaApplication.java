e34ce9e17bcffb3f3e94fc1e9ed9bd2b025f0e8c
Joao Teixeira <joao.teixeira.dev@gmail.com>
2023-09-26 18:26:23 -0300

    initial configs

---
+++ backoffice-api-java/src/main/java/com/backoffice_api_java/api/BackofficeApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.backoffice_api_java.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofficeApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackofficeApiJavaApplication.class, args);
+	}
+
+}
---
+++ backoffice-api-java/src/test/java/com/backoffice_api_java/backofficeapijava/BackofficeApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backoffice_api_java.backofficeapijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofficeApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
